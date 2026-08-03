afa90580b84f398d7a115cba2de34db6ac8737cb
hussainali930815500-afk <hussainali930815500@gmail.com>
2026-08-03 08:12:21 -0700

    Add Screen sealed class for navigation routes

---
+++ navigation/Screen.kt
@@ -0,0 +1,14 @@
+package dev.pranav.applock.core.navigation
+
+sealed class Screen(val route: String) {
+    object AppIntro : Screen("app_intro")
+    object SetPassword : Screen("set_password")
+    object SetPasswordPattern : Screen("set_password_pattern")
+    object SetPasswordAlphanumeric : Screen("set_password_alphanumeric")
+    object ChangePassword : Screen("change_password")
+    object Main : Screen("main")
+    object PasswordOverlay : Screen("password_overlay")
+    object Settings : Screen("settings")
+    object TriggerExclusions : Screen("trigger_exclusions")
+    object AntiUninstall: Screen("anti_uninstall")
+}
